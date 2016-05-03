1b4aa11eb3f605275ecd0fe6605ad09ca5ceb66f
jake steinberg <jakesteinberg@gmail.com>
2016-05-03 11:09:58 -0700

    add recycler view and a list item

--- app/src/main/java/com/epicodus/discussionforum/ui/MessagesListActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/ui/MessagesListActivity.java
@@ -1,17 +1,25 @@
 package com.epicodus.discussionforum.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.epicodus.discussionforum.R;
+import com.epicodus.discussionforum.models.Message;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MessagesListActivity extends AppCompatActivity {
-    @Bind(R.id.messagesListView) ListView mMessagesListView;
+    @Bind(R.id.messagesListView)
+    ListView mMessagesListView;
+    public ArrayList<Message> mMessages = new ArrayList<>();
+    public static final String TAG = MessagesListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,19 @@ public class MessagesListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_messages_list);
         ButterKnife.bind(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, )
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        getMessages(message);
+    }
+
+    private void getMessages(String message) {
+
+        String[] messageNames = new String[mMessages.size()];
+        for (int i = 0; i < messageNames.length; i++) {
+            messageNames[i] = mMessages.get(i).getMessage();
+        }
+        ArrayAdapter adapter = new ArrayAdapter(MessagesListActivity.this, android.R.layout.simple_list_item_1, messageNames);
+        mMessagesListView.setAdapter(adapter);
+
     }
 }
