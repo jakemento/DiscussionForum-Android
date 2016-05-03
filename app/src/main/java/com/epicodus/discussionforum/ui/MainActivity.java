bcfd17bb0e33091618368470eec701fc3f9009ed
jake steinberg <jakesteinberg@gmail.com>
2016-05-03 09:10:47 -0700

    add a new activity for posting a message

--- app/src/main/java/com/epicodus/discussionforum/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/ui/MainActivity.java
@@ -1,15 +1,33 @@
 package com.epicodus.discussionforum.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.epicodus.discussionforum.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.newMessageButton) Button mNewMessageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    mNewMessageButton.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick (View v) {
+            Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+            startActivity(intent);
+        }
+    });
+
+
     }
 }
