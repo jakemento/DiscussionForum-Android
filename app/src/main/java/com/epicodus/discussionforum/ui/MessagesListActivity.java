8685b52845b9b2ca3d6f9dda1f103b604786f916
jake steinberg <jakesteinberg@gmail.com>
2016-05-03 11:55:27 -0700

    update message list activity...incomplete

--- app/src/main/java/com/epicodus/discussionforum/ui/MessagesListActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/ui/MessagesListActivity.java
@@ -3,11 +3,14 @@ package com.epicodus.discussionforum.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.epicodus.discussionforum.R;
+import com.epicodus.discussionforum.adapters.MessageListAdapter;
 import com.epicodus.discussionforum.models.Message;
 
 import java.util.ArrayList;
@@ -16,10 +19,12 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MessagesListActivity extends AppCompatActivity {
-    @Bind(R.id.messagesListView)
-    ListView mMessagesListView;
-    public ArrayList<Message> mMessages = new ArrayList<>();
     public static final String TAG = MessagesListActivity.class.getSimpleName();
+    @Bind(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    private MessageListAdapter mAdapter;
+
+    public ArrayList<Message> mMessages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +34,22 @@ public class MessagesListActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra("message");
+
         getMessages(message);
     }
 
     private void getMessages(String message) {
 
-        String[] messageNames = new String[mMessages.size()];
-        for (int i = 0; i < messageNames.length; i++) {
-            messageNames[i] = mMessages.get(i).getMessage();
-        }
-        ArrayAdapter adapter = new ArrayAdapter(MessagesListActivity.this, android.R.layout.simple_list_item_1, messageNames);
-        mMessagesListView.setAdapter(adapter);
+                MessagesListActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mAdapter = new MessageListAdapter(getApplicationContext(), mMessages);
+                mRecyclerView.setAdapter(mAdapter);
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MessagesListActivity.this);
+                mRecyclerView.setLayoutManager(layoutManager);
+                mRecyclerView.setHasFixedSize(true);
+            }
 
+        });
     }
 }
